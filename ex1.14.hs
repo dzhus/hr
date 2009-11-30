dfa61f0cf93b1f572b12d4bb345e14e52d73c8c7
Dmitry Dzhus <dima@sphinx.net.ru>
2009-11-30 22:54:58 +0300

    Ex 1.14: `blowup`

---
+++ ex1.14.hs
@@ -0,0 +1,14 @@
+module BLOWUP
+
+where
+
+blowup :: String -> String
+blowup s = blowup' s 1
+
+repeat' :: Char -> Integer -> String
+repeat' c 0 = []
+repeat' c n = c : repeat' c (n - 1)
+
+blowup' :: String -> Integer -> String
+blowup' [] d = []
+blowup' (x:xs) d = repeat' x d ++ blowup' xs (d + 1)
